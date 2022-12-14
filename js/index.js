e910ebe0b443b2652de651f573472a4fda03fbae
SandraMatos9 <matos.sandrarodrigues@email.com>
2022-12-14 13:34:25 -0300

    atualização

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 const produtosPintura = [
     {
       id: 1,
-      img: "../img/acrilico1.png",
+      img: "./img/acrilico1.png",
       nameItem: "Sonhadora",
       description:
         "Decore seu ambiente com essa tela pintada em acrílico de uma jovem sonhadora!",
@@ -13,7 +13,7 @@ const produtosPintura = [
     },
     {
       id: 2,
-      img: "../img/acrilico2.png",
+      img: "./img/acrilico2.png",
       nameItem: "Contemplando",
       description:
       "Decore seu ambiente com essa tela pintada em acrílico de uma moça contemplando...",
@@ -23,7 +23,7 @@ const produtosPintura = [
     },
     {
       id: 3,
-      img: "../img/guache1.png",
+      img: "./img/guache1.png",
       nameItem: "Sonhando",
       description:
       "Decore seu ambiente com essa tela pintada em guache retratando alguém sonhando.",
@@ -33,7 +33,7 @@ const produtosPintura = [
     },
     {
       id: 4,
-      img: "../img/guache2.png",
+      img: "./img/guache2.png",
       nameItem: "Lysis",
       description:
         "Quadro de Lysis em tinta guache.",
@@ -43,7 +43,7 @@ const produtosPintura = [
     },
     {
       id: 5,
-      img: "../img/oleo1.png",
+      img: "./img/oleo1.png",
       nameItem: "Banho de tinta",
       description:
         "Sua casa vai ganhar muita cor com esse quadro inspirado num cachorrinho a óleo!",
@@ -53,7 +53,7 @@ const produtosPintura = [
     },
     {
       id: 6,
-      img: "../img/oleo2.png",
+      img: "./img/oleo2.png",
       nameItem: "Curiosodade",
       description:
       "Essa pintura a óleo nos revela uma das características mais conhecida dos gatos: a curiosidade!",
